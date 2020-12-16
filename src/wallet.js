80140e7c5f4e61e96ffb981c4efc24e9bc5033c2
pcatana <catanapetru@gmail.com>
2020-12-16 16:42:35 +0000

    added network provider param to wallet constructor [ch7732] "

--- src/wallet.js
+++ src/wallet.js
@@ -3,70 +3,20 @@
 * As an alternative, user can also paste in his private key
 */
 
-// const fs = require('fs');
-// const path = require('path');
-// const ethers = require('ethers');
-
 import { ethers } from 'ethers';
 import fs from 'fs';
 import path from 'path';
+import network from './singleton/network';
 
-// one needs to pass in the filepath to the wallet and password that is relative to the working directory
-
-// const jsonWallet = '/tests/testwallet.json';
-// const walletPass = '/tests/jsonpassword.txt';
-
-
-
-
-// // Reads Password from the JSONWallet Password File
-// function getPassword() {    
-//     try {
-//         // let data =  fs.readFileSync(walletPass, 'utf8');
-//         const workingDirPath = path.resolve();
-//         // const fullpath = path.join(workingDirPath,'auction-demo-keeper', walletPass);
-//         const fullpath = path.join(workingDirPath, walletPass);
-//         const data = fs.readFileSync(fullpath, 'utf8').toString();
-//         console.log('data ', data);
-//         return data;
-
-//     } catch (error) {
-//         console.log('Error reading pass file', error.stack);
-//     }
-    
-// }
-// // getPassword();
-
-// async function getWallet() {
-//     try {
-//         const workingPath = path.resolve();
-//         const fullPath = path.join(workingPath, jsonWallet);
-//         const readJSON = fs.readFileSync(fullPath, 'utf8');
-//         const JSONWallet = JSON.parse(readJSON);
-//         if (!JSONWallet) console.log('Error with reading JSON Wallet');
-//         const JSONWalletPassword = getPassword();
-//         const wallet = await new ethers.Wallet.fromEncryptedJson(JSON.stringify(JSONWallet), JSONWalletPassword);
-//         console.log('wallet', wallet);
-//         return wallet;
-//     } catch (error) {
-//         console.log(error);
-//     }
-
-// }
-
-// getWallet();
-
-//to do`
-// get test wallet from testchain pricat keys
-
-// transforn it into a class
 export default class Wallet {
     _passwordPath;
     _JSONKeystorePath;
+    _rpcURL;
 
-    constructor(passwordPath, JSONKeystorePath) {
+    constructor(passwordPath, JSONKeystorePath, rpcURL) {
         this._passwordPath = passwordPath;
         this._JSONKeystorePath = JSONKeystorePath;
+        this._rpcURL = rpcURL; 
     }
 
     
@@ -93,13 +43,12 @@ export default class Wallet {
 
             const JSONWalletPassword = this._getPassword();
             const wallet = await new ethers.Wallet.fromEncryptedJson(JSON.stringify(JSONWallet), JSONWalletPassword);
-            return wallet;
+
+            network.rpcURL = this._rpcURL;
+            const signer = new ethers.Wallet(wallet, network.provider);
+            return signer;
         } catch (error) {
             console.log(error);
         }
     }
 }
-
-// const wallet = new Wallet('/tests/jsonpassword.txt', '/tests/testwallet.json');
-// const signer = wallet.getWallet();
-// console.log('Signer ', signer);
