3dcb2bf1a2ac699a2c92061ec15b4c69fde3dfbe
pcatana <catanapetru@gmail.com>
2021-01-12 13:50:40 +0000

    created execute function [ch7476]

--- src/dex/oasisdex.js
+++ src/dex/oasisdex.js
@@ -4,13 +4,15 @@ import { ethers, BigNumber } from 'ethers';
 import supportMethodsAbi from '../../abi/MakerOtcSupportMethods.json';
 import matchingMarketAbi from '../../abi/MatchingMarket.json';
 import oasisCalleeAbi from '../../abi/CalleeMakerOtcDai.json';
+import clipperAbi from '../../abi/clipper.json';
+import Transact from '../transact';
 
 
 export default class oasisDexAdaptor {
-  _book=[];
-  _lastBlock=0;
+  _book = [];
+  _lastBlock = 0;
   _asset;
-  constructor (asset , callee ) {
+  constructor(asset, callee) {
     this._provider = network.provider;
     this._asset = asset;
     this._otcSupportMethods = new ethers.Contract(Config.vars.MakerOTCSupportMethods, supportMethodsAbi, this._provider);
@@ -22,28 +24,51 @@ export default class oasisDexAdaptor {
 
   fetch = async () => {
     const blockNumber = await this._provider.getBlockNumber();
-    if(blockNumber === this._lastBlock) return;
+    if (blockNumber === this._lastBlock) return;
 
     this._lastBlock = blockNumber;
-    const offers = await this._otcSupportMethods['getOffers(address,address,address)']( Config.vars.OasisDex,
-      Config.vars.dai, this._asset );
-    this._book = offers.ids.map( (v, i) => ( {id:v, payAmt: offers.payAmts[i], buyAmt:offers.buyAmts[i]} ) )
-      .filter(v=>(!(v.id.eq(0))));
+    const offers = await this._otcSupportMethods['getOffers(address,address,address)'](Config.vars.OasisDex,
+      Config.vars.dai, this._asset);
+    this._book = offers.ids.map((v, i) => ({ id: v, payAmt: offers.payAmts[i], buyAmt: offers.buyAmts[i] }))
+      .filter(v => (!(v.id.eq(0))));
   };
 
   baseBook = () => {
-    return this._book.map(entry => {return {
-      id:entry.id.toNumber(),
-      payAmt:ethers.utils.formatUnits(entry.payAmt),
-      buyAmt:ethers.utils.formatUnits(entry.buyAmt)}
+    return this._book.map(entry => {
+      return {
+        id: entry.id.toNumber(),
+        payAmt: ethers.utils.formatUnits(entry.payAmt),
+        buyAmt: ethers.utils.formatUnits(entry.buyAmt)
+      }
     });
   };
 
-  opportunity = ( price ) => {
+  opportunity = (price) => {
     //Total amount of collateral available for sale for a certain price
     return this._book
-      .filter ( v=> v.payAmt.div(v.buyAmt).gte(price.div(ethers.constants.WeiPerEther)))
-      .reduce( (previous, current) => previous.add(current.buyAmt), BigNumber.from(0));
+      .filter(v => v.payAmt.div(v.buyAmt).gte(price.div(ethers.constants.WeiPerEther)))
+      .reduce((previous, current) => previous.add(current.buyAmt), BigNumber.from(0));
   };
+
+
+  execute = async (_amt, _maxPrice, _minProfit, _profitAddr, _gemA, who, _signer) => {
+    
+    //encoding calldata
+    let typesArray = ['address', 'address', 'uint256'];
+    let abiCoder = ethers.utils.defaultAbiCoder;
+    let flashData = abiCoder.encode(typesArray, [_profitAddr, _gemA, minProfit]);
+    
+    let id = abiCoder.encode(['uint256'], [1]);
+    let amt = ethers.utils.parseEther(`${_amt}`);
+    let maxPrice = ethers.utils.parseEther(`${_maxPrice}`);
+    let minProfit = ethers.utils.parseEther(`${_minProfit}`);
+
+    const clipper = new ethers.Contract(Config.vars.clipper, clipperAbi, _signer.provider);
+    const take_transaction = await clipper.populateTransaction.take(id, amt, maxPrice, who, flashData );
+    console.log('Take_Transaction ', take_transaction);
+    const txn = new Transact(take_transaction, _signer, Config.vars.txnReplaceTimeout);
+    await txn.transact_async();
+
+  }
 
 }
