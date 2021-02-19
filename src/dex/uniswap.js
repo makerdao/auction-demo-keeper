090b8345d740c237bec809aae728dcfd567bfe46
pcatana <catanapetru@gmail.com>
2021-02-19 14:38:23 +0000

    init uniswap.js

---
+++ src/dex/uniswap.js
@@ -0,0 +1,9 @@
+import Config from '../singleton/config';
+import network from '../singleton/network';
+import { ethers, BigNumber } from 'ethers';
+// import uniswap contract ABis
+
+
+export default class UniswapAdaptor {
+
+}
