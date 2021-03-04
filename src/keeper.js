9217f06d026137b5cf5c18a9f6aa88cc602aa336
Kenton Prescott <kentonprescott@gmail.com>
2021-03-04 10:39:32 -0700

    added comments and fixed the config

--- src/keeper.js
+++ src/keeper.js
@@ -27,23 +27,33 @@ export default class keeper {
     _this=this;
   }
 
+  // Check if there's an opportunity in Uniswap & OasisDex to profit with a LIQ2.0 flash loan
   _opportunityCheck ( collateral, oasis, uniswap, clip ) {
     console.log('Check auction opportunities for ' + collateral.name);
+
+    // Fetch the orderbook from OasisDex & all the active auctions
     Promise.all( [oasis.fetch(), clip.activeAuctions()] ).then( (res) => {
       const activeAuctions = res[1];
 
       console.log('Active auctions qty: ' + activeAuctions.length);
 
+      // Look through the list of active auctions
       activeAuctions.forEach( auction => {
 
+        // Pass in the entire auction size into Uniswap and store the Dai proceeds form the trade
         Promise.all( [uniswap.fetch(auction.lot)] ).then(() => {
 
-          const priceWithProfit = auction.price.mul(Config.vars.minProfitNum).div(Config.vars.minProfitDen)
+          // Find the minimum effective exchange rate between collateral/Dai
+          // e.x. ETH price 1000 DAI -> minimum profit of 1% -> new ETH price is 1000*1.01 = 1010
+          const priceWithProfit = auction.price.mul(Config.vars.minProfitPercentage);
 
+          // Find the amount of collateral that maximizes the amount of profit captured
           const oasisDexAvailability = oasis.opportunity(priceWithProfit);
 
+          // Return the proceeds from the Uniswap market trade; proceeds were queried in uniswap.fetch()
           const uniswapProceeds = uniswap.opportunity();
 
+          // Determine how much collateral we can trade on OasisDex
           const oasisSize = oasisDexAvailability.gt(auction.lot) ? auction.lot : oasisDexAvailability;
 
           console.log(`Auction # ${auction.id} \n
@@ -54,11 +64,12 @@ export default class keeper {
 
           //TODO: Determine if we already have a pending bid for this auction
 
-          // Check if there's a Dai profit from Uniswap
+          // Check if there's a Dai profit from Uniswap by selling the entire auction
           if (uniswapProceeds.receiveAmount > priceWithProfit.mul(auction.lot)) {
             uniswap.execute( auction.id, auction.lot, auction.price );
 
-          // If there's not a profit from Uniswap, check OasisDex
+          // If there's not a profit from Uniswap, use Oasis to sell a portion of
+          // the collateral that maximizes the Dai profit
           } else if (oasisSize > 0) {
             oasis.execute( auction.id, Math.min(oasisSize, auction.lot), auction.price );
           }
@@ -69,6 +80,7 @@ export default class keeper {
     });
   }
 
+  // Initialize the Clipper, OasisDex, and Uniswap JS wrappers
   async _clipperInit( collateral ) {
     const oasis = new oasisDexAdaptor( collateral.erc20addr, collateral.oasisCallee );
     const uniswap = new UniswapAdaptor( collateral.erc20addr, collateral.uniswapCallee );
@@ -76,6 +88,7 @@ export default class keeper {
     await oasis.fetch();
     await clip.init();
 
+    // Initialize the loop where an opportunity is checked at a perscribed cadence (Cofnig.delay)
     const timer = setInterval(() => {this._opportunityCheck(collateral,oasis,uniswap,clip)}, Config.vars.delay * 1000);
     return({oasis, uniswap, clip, timer});
   }
