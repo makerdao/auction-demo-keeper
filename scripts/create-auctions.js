efb712c074eba792bf621b856bd860dfcdaf2c79
pcatana <catanapetru@gmail.com>
2021-03-16 19:23:06 +0000

    semi-working vault creation

--- scripts/create-auctions.js
+++ scripts/create-auctions.js
@@ -6,59 +6,94 @@ import { McdPlugin, ETH, DAI, LINK } from '@makerdao/dai-plugin-mcd';
     const maker = await Maker.create('http', {
         plugins: [McdPlugin],
         url: 'https://kovan.infura.io/v3/c7c45c0e046e49feb141d72680af4f0a',
-        privateKey: 'INSERT_PRIVATE_EKY'
+        privateKey: ''
     });
 
-    console.log('Current Wallet Address: ',maker.currentAddress());
+    console.log('Current Wallet Address: ', maker.currentAddress());
 
     console.log('Ensure there is proxy address');
     await maker.service('proxy').ensureProxy();
     console.log('Proxy Address: ', await maker.service('proxy').getProxyAddress());
 
     //create risky vault using ETH-A 
+    console.log(' ');
     console.log('Creating risky vault');
     const manager = maker.service('mcd:cdpManager');
-    let vault = null;
-    try{
-        vault = await manager.openLockAndDraw(
-            'LINK-A',
-            LINK(16.49),
-            DAI(100.06)
-        );
-        vault;
-    }catch(error) {
-        console.log(error);
-    }
+    
+    const vault = await manager.open('LINK-A');
+    let vaultId = vault.id;
+    console.log('Vault ID', vaultId);
+
+    console.log('Locking collateral');
+    await manager.lock(
+        vault.id,
+        'LINK-A',
+        LINK(16.49)
+    );
+
+    console.log(' ');
+    console.log('Dripping LINK-A JUG');
+    await maker
+        .service('smartContract')
+        .getContract('MCD_JUG')
+        .drip('0x4c494e4b2d410000000000000000000000000000000000000000000000000000');
+
+
+    // Refreshing vault data
+    vault.reset();
+    await vault.prefetch();
 
     // Reading Vault Information
-    const vaultId = vault.id;
+    let managedVault = await manager.getCdp(vaultId);
+    managedVault.reset();
+    await managedVault.prefetch();
     const vaultUrnAddr = await manager.getUrn(vaultId);
     console.log('Vault: Urn Address', vaultUrnAddr);
 
-    const managedVault = await manager.getCdp(vaultId);
-    console.log('Managed Vault: ', managedVault);
+    const amtDai = await managedVault.daiAvailable._amount;
 
     console.log('Collateral Value: ', managedVault.collateralValue._amount);
+    console.log('DAI Available to Generate', managedVault.daiAvailable._amount);
     console.log('Debt Value: ', managedVault.debtValue._amount);
     console.log('Collateralization Ratio ', managedVault.collateralizationRatio._amount);
     console.log('Liquidation Price ', managedVault.liquidationPrice._amount);
     console.log('Is Vault safe? ', managedVault.isSafe);
 
     console.log(' ');
-    console.log('Dripping JUG');
+    console.log(`Drawing ${amtDai} from Vault #${vaultId}`);
+
+    try{
+        let drawDai = await manager.draw(
+            vaultId,
+            'LINK-A',
+            DAI(`${amtDai}`)
+        );
+        drawDai;
+    }catch(error){
+        console.error(error);
+    }
+
+    console.log(' ');
+    console.log('Dripping LINK-A JUG');
     await maker
         .service('smartContract')
         .getContract('MCD_JUG')
         .drip('0x4c494e4b2d410000000000000000000000000000000000000000000000000000');
-    
-    
 
+        
+    //Refreshing Vault Data
+    managedVault.reset();
+    await managedVault.prefetch();
+
+    // Getting Updated state from Vault
+    console.log(' ');
     console.log('Collateral Value: ', managedVault.collateralValue._amount);
+    console.log('DAI Available to Generate', managedVault.daiAvailable._amount);
     console.log('Debt Value: ', managedVault.debtValue._amount);
     console.log('Collateralization Ratio ', managedVault.collateralizationRatio._amount);
     console.log('Liquidation Price ', managedVault.liquidationPrice._amount);
     console.log('Is Vault safe? ', managedVault.isSafe);
-    
+
 
     process.kill(process.pid, 'SIGTERM');
 })();
