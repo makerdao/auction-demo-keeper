0e955ccd703db0f423f49a25cc0c65d8fb4b1b31
Njoku Emmanuel <kalunjoku123@gmail.com>
2021-03-25 15:14:55 +0100

    started tests

---
+++ tests/clipper.test.js
@@ -0,0 +1,34 @@
+/**
+ * @jest-environment node
+ */
+ import { ethers } from 'ethers';
+ import Config from '../src/singleton/config';
+ import network from '../src/singleton/network';
+ import { expect, it } from '@jest/globals';
+ import oasisDexAdaptor from '../src/dex/oasisdex';
+ import config from '../config/kovan.json';
+ import Clipper from '../src/clipper';
+ import Wallet from '../src/wallet';
+ import { Transact } from '../src/transact';
+ import daiAbi from '../abi/Dai.json';
+ 
+ network.rpcURL = 'https://kovan.infura.io/v3/c7c45c0e046e49feb141d72680af4f0a';
+ Config.vars = config;
+ console.log('network.rpcURL', network.provider);
+ console.log('CONFIG ',Config.vars);
+ 
+ 
+ const sleep = async function (delay) { await new Promise((r) => setTimeout(r, delay * 1000)); };
+
+ const privateKey = '0x474BEB999FED1B3AF2EA048F963833C686A0FBA05F5724CB6417CF3B8EE9697E';
+console.log('Network Provider: ', network.provider);
+const signer = new ethers.Wallet(privateKey, network.provider);
+console.log('Address: ' + signer.address);
+ 
+it("Initializes the clipper and listen for active auctions" = async() => {
+    const clipper = new Clipper('LINK-A');
+
+    console.log(clipper, "clipper")
+    
+    await clipper.init();
+})
