5dfac5d5200f747a47809ba872adbe21176fc7c7
pcatana <catanapetru@gmail.com>
2021-04-16 15:58:28 +0100

    added claim tip feature in keeper - keeper is now able to withdraw any rewards it receives from redoing expired auctions

--- src/vat.js
+++ src/vat.js
@@ -1,32 +1,33 @@
 import network from './singleton/network.js';
-import { ethers } from 'ethers';
+import { ethers, BigNumber } from 'ethers';
 import Config from './singleton/config.js';
+import vatAbi from '../abi/vat.json';
 import { Transact, GeometricGasPrice } from './transact.js';
 
 
-const vatAbi = [
-    {
-        "constant": true,
-        "inputs": [
-            { "internalType": "address", "name": "", "type": "address" },
-            { "internalType": "address", "name": "", "type": "address" }
-        ],
-        "name": "can",
-        "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "constant": false,
-        "inputs": [{ "internalType": "address", "name": "usr", "type": "address" }],
-        "name": "hope",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }
-];
+// const vatAbi = [
+//     {
+//         "constant": true,
+//         "inputs": [
+//             { "internalType": "address", "name": "", "type": "address" },
+//             { "internalType": "address", "name": "", "type": "address" }
+//         ],
+//         "name": "can",
+//         "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+//         "payable": false,
+//         "stateMutability": "view",
+//         "type": "function"
+//     },
+//     {
+//         "constant": false,
+//         "inputs": [{ "internalType": "address", "name": "usr", "type": "address" }],
+//         "name": "hope",
+//         "outputs": [],
+//         "payable": false,
+//         "stateMutability": "nonpayable",
+//         "type": "function"
+//     }
+// ];
 
 
 const clipperAllowance = async (clipperAddress, _signer) => {
@@ -40,11 +41,60 @@ const clipperAllowance = async (clipperAddress, _signer) => {
             console.log('HOPING CLIPPER IN VAT');
             const hope_transaction = await vatContract.populateTransaction.hope(clipperAddress);
             const txn = new Transact(hope_transaction, _signer, Config.vars.txnReplaceTimeout, gasStrategy);
-            await txn.transact_async();
+            const response = await txn.transact_async();
+            console.log(`Hoped Cliipper Contract in VAT ${response.hash}`);
         }
     } catch (error) {
         console.error(error);
     }
 };
 
-export default clipperAllowance;
+const checkVatBalance = async (_signer) => {
+    const decimals27 = ethers.utils.parseEther('1000000000');
+
+    const vatContract = new ethers.Contract(Config.vars.vat, vatAbi, network.provider);
+    const balance = await vatContract.dai(_signer.address);
+    const daiToWithdraw = balance.div(decimals27);
+
+    if(ethers.utils.formatUnits(daiToWithdraw) > 0) {
+        await withdrawExtraDai(_signer, daiToWithdraw);
+    }
+};
+
+const withdrawExtraDai = async (_signer, wadAmt) => {
+    const daiJoinContract = new ethers.Contract(Config.vars.daiJoin, daiJoinAbi, network.provider);
+    const initial_price = await _signer.getGasPrice();
+    const gasStrategy = new GeometricGasPrice(initial_price.toNumber(), Config.vars.txnReplaceTimeout, Config.vars.dynamicGasCoefficient);
+
+    try {
+        console.log(`Withdrwaing tipped ${ethers.utils.formatUnits(wadAmt)} Dai from VAT`);
+        const exit_transaction = await daiJoinContract.populateTransaction.exit(_signer.address, wadAmt);
+        const txn = new Transact(exit_transaction, _signer, Config.vars.txnReplaceTimeout, gasStrategy);
+        const response = await txn.transact_async();
+        console.log(`Withdrew tipped Dai ${response.hash}`);
+    }catch(error){
+        console.error(error);
+    }
+
+};
+
+
+const daiJoinAbi = [
+    {
+        "constant": false,
+        "inputs": [
+            { "internalType": "address", "name": "usr", "type": "address" },
+            { "internalType": "uint256", "name": "wad", "type": "uint256" }
+        ],
+        "name": "exit",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
+
+export {
+    clipperAllowance,
+    checkVatBalance
+};
