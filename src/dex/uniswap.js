e1d89a261a03724f9fd8da9438e215b393d63423
pcatana <catanapetru@gmail.com>
2021-05-17 16:26:55 +0100

    fix uniswap WBTC-A eval

--- src/dex/uniswap.js
+++ src/dex/uniswap.js
@@ -1,6 +1,6 @@
 import Config from '../singleton/config.js';
 import network from '../singleton/network.js';
-import { ethers } from 'ethers';
+import { ethers, BigNumber } from 'ethers';
 import uniswapRouter from '../../abi/UniswapV2Router02.json';
 import uniswapCalleeAbi from '../../abi/UniswapV2CalleeDai.json';
 
@@ -10,7 +10,8 @@ export default class UniswapAdaptor {
         receiveAmount: ''
     };
     _lastBlock = 0;
-    _collateralName = ''
+    _collateralName = '';
+    _decimals10 = BigNumber.from('10000000000');
 
     constructor(assetAddress, callee, collateralName) {
         this._provider = network.provider;
@@ -21,14 +22,20 @@ export default class UniswapAdaptor {
     }
 
     // ilkAmount in WEI
-    fetch = async (ilkAmount) => {
+    fetch = async (_ilkAmount) => {
+        let ilkAmount;
+        if (this._collateralName === 'WBTC-A') {
+            ilkAmount = BigNumber.from(_ilkAmount).div(this._decimals10);
+        } else {
+            ilkAmount = _ilkAmount;
+        }
         try {
             const blockNumber = await this._provider.getBlockNumber();
             if (blockNumber === this._lastBlock) return;
             this._lastBlock = blockNumber;
 
             const offer = await this._uniswap.getAmountsOut(ilkAmount, Config.vars.collateral[this._collateralName].uniswapRoute);
-            this._book.sellAmount = ethers.utils.formatUnits(offer[0]);
+            this._book.sellAmount = this._collateralName === 'WBTC-A' ? ethers.utils.formatUnits(offer[0].mul(this._decimals10)) : ethers.utils.formatUnits(offer[0]);
             this._book.receiveAmount = ethers.utils.formatUnits(offer[offer.length - 1]);
         } catch (e) {
             console.log('uniswap error:', e);
