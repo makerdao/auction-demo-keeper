bea7d4fc1cff68716dc17aa709d89caa3b545e19
pcatana <catanapetru@gmail.com>
2021-05-31 11:38:34 +0100

    setup todos

--- src/multicall.js
+++ src/multicall.js
@@ -8,7 +8,23 @@ export default class Multicall {
     _multi;
     _clipLinkA;
     _kicks;
+    _clippers = [];
+    results = {
 
+    }
+    // TODO
+    // look at ilkRegistry - check Clippers - list/iterate - > grab xlap element
+    // query each clipper active auctions
+    // for each acction grab action details...
+    // hybrid of multicall/events - check for events and update auction state
+
+    // TODO
+    // automatically update config.json with latest contract addresses.
+
+    // TODO
+    // Update config.json ILK CLipper addresses with ilk_registry
+    // get xlip of each listed collateral in config
+    // update config with latest clip address 
     constructor() {
         this._provider = network.provider;
         this.init();
@@ -25,6 +41,17 @@ export default class Multicall {
     init() {
         this._multi = this.build(Config.vars.multicall, 'Multicall');
         this._clipLinkA = this.build(Config.vars.collateral['LINK-A'].clipper, 'clipper');
+        console.log('clippers', this._clippers);
+    }
+
+    setupClipperContracts() {
+        for (const name in Config.vars.collateral) {
+            if (Object.prototype.hasOwnProperty.call(Config.vars.collateral, name)) {
+                const collateral = Config.vars.collateral[name];
+                let clipContract = this.build(collateral.clipper, 'clipper');
+                this._clippers.push({ name: collateral.name, contract: clipContract });
+            }
+        }
     }
 
     getInfo = async (blockNumber) => {
